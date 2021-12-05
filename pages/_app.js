564222951c490d238fb2a2478a5d511aa4c6a4e5
seanisoverhere <sean_choon@hotmail.com>
2021-12-05 18:44:26 +0800

    fix: provider to be client side rendered

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import 'tailwindcss/tailwind.css'
-import { SessionProvider } from "next-auth/react"
 
-export default function App({
-  Component, pageProps: { session, ...pageProps }
-}) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps}/>
-    </SessionProvider>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
--- pages/login.js
+++ pages/login.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getProviders, signIn } from "next-auth/react";
 
-// getProviders().then((data) => console.log(data.spotify));
+const Login = () => {
+  const [clientProviders, setClientProviders] = useState();
 
-const Login = ({ providers }) => {
+  useEffect(() => {
+    getProviders().then((res) => setClientProviders(res));
+  }, []);
+
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center bg-black min-h-screen w-full">
       <img
         className="w-52 mb-5"
         src="https://links.papareact.com/9xl"
         alt="spotify logo"
       />
 
-      {providers && Object.values(providers).map((provider) => (
-        <buton>HI</buton>
-      ))}
+      {clientProviders &&
+        Object.values(clientProviders).map((provider) => (
+          <div key={provider.name}>
+            <button
+              className="bg-green-400 text-white p-3 rounded-lg mt-4"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Login with {provider.name}
+            </button>
+          </div>
+        ))}
     </div>
   );
 };
 
 export default Login;
-
-export async function getServerSideProps() {
-
-  const providers = getProviders().then(data => console.log(data))
-  console.log("INSIDE FETCH")
-
-  return {
-    props: {
-      providers: providers
-    },
-  };
-}
